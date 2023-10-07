194bfd4547b7c8eddb5781f93be443716e6a3ba9
Antonio <antoniooficial2004@gmail.com>
2023-10-07 08:17:48 +0200

    feat: wifi verification change the main text content

--- App.js
+++ App.js
@@ -1,10 +1,25 @@
+import { useState } from "react";
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import NetInfo from "@react-native-community/netinfo";
 
 export default function App() {
+
+  const [wifiState, setWifiState] = useState(false);
+
+  NetInfo.fetch()
+  .then( state => {
+    console.log("Connection Type: ", state.type);
+    console.log("Is connected? ", state.isConnected);
+
+    setWifiState(state.isConnected);
+  });
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>
+        { wifiState ? "+ Connected" : "- Disconnected" }
+      </Text>
       <StatusBar style="auto" />
     </View>
   );
